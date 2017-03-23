cecd1f25bbfcf081fe032747e531efa647992ecd
lijiangbo <lijiangbo@howzhi.com>
2017-03-23 20:22:10 +0800

    材料题更改

---
+++ backend/web/js/short-long.js
@@ -0,0 +1,10 @@
+$element = $('.shor-long');
+$element.on('click', '.short-text', function() {
+    var $short = $(this);
+    $short.slideUp('fast').parents('.short-long-text').find('.long-text').slideDown('fast');
+});
+
+$element.on('click', '.long-text', function() {
+    var $long = $(this);
+    $long.slideUp('fast').parents('.short-long-text').find('.short-text').slideDown('fast');
+});
